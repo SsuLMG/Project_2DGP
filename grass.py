c57acead5f934e0eed3cac49335629dc1b7f7ec2
SsuLMG <seanseol@naver.com>
2023-11-03 21:13:28 +0900

    stadium 띄우기

--- grass.py
+++ grass.py
@@ -2,13 +2,14 @@ from pico2d import *
 
 class Grass:
     def __init__(self):
-        self.image = load_image('grass.png')
+        self.image = load_image('stadium.png')
 
     def update(self):
         pass
 
     def draw(self):
-        self.image.draw(400, 30)
-        self.image.draw(1200, 30)
+        self.image.draw(1172/2, 764/2)
+        #self.image.draw(1200, 30)
 
 
+#1172 x 1093
