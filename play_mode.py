66f0dae7caf26acb4abc24bcdfc21496a01d913e
SsuLMG <seanseol@naver.com>
2023-11-16 17:59:59 +0900

    title_mode 마무리

--- play_mode.py
+++ play_mode.py
@@ -5,6 +5,7 @@ import game_framework
 
 import game_world
 from grass import Grass
+import title_mode
 #from boy import Boy
 from ball import Ball
 from zombie import Zombie
@@ -19,7 +20,7 @@ def handle_events():
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            game_framework.quit()
+            game_framework.change_mode(title_mode)
         else:
             hitter.handle_event(event)
             pitcher.handle_event(event)
--- title_mode.py
+++ title_mode.py
@@ -1,14 +1,34 @@
-from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE
+from pico2d import load_image, delay, clear_canvas, update_canvas, get_events, get_time
+from sdl2 import SDL_KEYDOWN, SDL_QUIT, SDLK_ESCAPE, SDLK_SPACE
 
 import game_framework
-from pico2d import load_image, delay, clear_canvas, update_canvas, get_events, get_time
+import play_mode
+
 
 def init():
     global image
+    global running
+    global logo_start_time
     image = load_image('title.png')
+    running = True
+    logo_start_time = get_time()
+
 def finish():
     global image
     del image
+
+def update():
+    global running
+    global logo_start_time
+    if get_time() - logo_start_time >= 5.0:
+        logo_start_time = get_time()
+        game_framework.quit()
+
+def draw():
+    clear_canvas()
+    image.draw(1172/2, 764/2)
+    update_canvas()
+
 def handle_events():
     events = get_events()
     for event in events:
@@ -16,7 +36,5 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
-def draw():
-    clear_canvas()
-    image.draw(400,300)
-    update_canvas()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+            game_framework.change_mode(play_mode)
