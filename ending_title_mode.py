5f380cbe4b082444572e61a52d1369e974ce93cb
SsuLMG <seanseol@naver.com>
2023-12-03 22:31:33 +0900

    Create ending_title_mode.py

---
+++ ending_title_mode.py
@@ -0,0 +1,40 @@
+from pico2d import load_image, delay, clear_canvas, update_canvas, get_events, get_time
+from sdl2 import SDL_KEYDOWN, SDL_QUIT, SDLK_ESCAPE, SDLK_SPACE
+
+import game_framework
+import play_mode
+
+
+def init():
+    global image
+    global running
+    global logo_start_time
+    image = load_image('title.png')
+    running = True
+    logo_start_time = get_time()
+
+def finish():
+    global image
+    del image
+
+def update():
+    global running
+    global logo_start_time
+    if get_time() - logo_start_time >= 5.0:
+        logo_start_time = get_time()
+        game_framework.quit()
+
+def draw():
+    clear_canvas()
+    image.draw(1172/2, 764/2)
+    update_canvas()
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+            game_framework.change_mode(play_mode)
