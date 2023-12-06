7680f35fd18530daf8d4c7a6c3375211043ff159
SsuLMG <seanseol@naver.com>
2023-12-06 21:07:48 +0900

    등장곡

--- grass.py
+++ grass.py
@@ -1,9 +1,14 @@
 from pico2d import *
 
 class Grass:
+    enter_sound = None
+
     def __init__(self):
         self.image = load_image('stadium.png')
         self.font = load_font('ENCR10B.TTF', 16)
+        self.enter_sound = load_wav('enter_sound.wav')
+        self.enter_sound.set_volume(32)
+        self.enter_sound.play()
 
     def update(self):
         pass
@@ -11,6 +16,8 @@ class Grass:
     def draw(self):
         self.image.draw(1172/2, 764/2)
         #self.image.draw(1200, 30)
+
+
 
 
 
