b5cdcf7a66998ac0ebc6d80b1ddf26ea8cc03745
SsuLMG <seanseol@naver.com>
2023-12-06 21:08:14 +0900

    아웃 사운드

--- hitball.py
+++ hitball.py
@@ -15,6 +15,7 @@ def Lerp(A, B, Alpha):
 
 class HitBall:
     image = None
+    hit_sound = None
     def __init__(self, x = 495, y = 50, velocity = 1):
         if HitBall.image == None:
             HitBall.image = load_image('baseball.png')
@@ -23,12 +24,15 @@ class HitBall:
         self.final_ball_y = 500  # 230
         game_world.add_object(self)
         game_world.add_collision_pair('fielder:hitball', None, self)
+        HitBall.out_sound = load_wav('out.wav')
+        HitBall.out_sound.set_volume(32)
+
 
     def draw(self):
         self.x = Lerp(self.x, self.final_ball_x,0.01)
         self.y = Lerp(self.y, self.final_ball_y,0.01)
         self.image.draw(self.x, self.y)
-        draw_rectangle(*self.get_bb())
+        #draw_rectangle(*self.get_bb())
 
     def update(self):
         score = Define.instance.score
@@ -66,7 +70,10 @@ class HitBall:
     def handle_collision(self, group, other):
         match group:
             case 'fielder:hitball':
+                HitBall.out_sound.play()
                 game_world.remove_object(self)
+
+
 
     def final_x(self):
         return self.x
